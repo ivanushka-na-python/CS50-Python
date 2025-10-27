a4aba9613c7cb375f404053f02b37fed1528510b
Ivan Ushakov <Ivanov.ivan.krd@gmail.com>
2025-10-27 18:45:18 +0300

    end of Lesson_0

--- Lessons/Lesson_0.py
+++ Lessons/Lesson_0.py
@@ -2,14 +2,49 @@
 # аргументы - входные данные для функции 
 
 '''
-hello - три апострофа или двойных кавычек - тоже коммент - н=о использовать не удобно, если только не писать на нескольких строках
+hello - три апострофа или двойных кавычек - тоже коммент - но использовать не удобно, если только не писать на нескольких строках
 то что мы можем передать в func - параметры, что мы передаем в func - аргументы
 sep и end - именованные параметры 
 '''
 
-# name = input('Whats your name? ')
+# name = input('Whats your name? ').strip(',').title()
+# # name = name.strip(',').title() # strip - это метод, не функция
 # print(f'hello, {name}')
 
-import os
-print("Текущая рабочая директория:", os.getcwd()) # узнаем рабочую директорию 
 
+# name = input('Whats your name? ').strip().title() 
+# first_name, surname = name.split(' ')
+# print(f'hello, {first_name}')
+
+# x = input('x ')
+# y = input('y ')
+# z = int(x) + int(y) # преобразуем строки в инт, любой ввод с клавы - строка
+# print(z)
+
+# x = int(input('x '))
+# y = int(input('y '))
+# print(x + y)
+
+
+# x = float(input('x '))
+# y = float(input('y '))
+# z = round(x / y, 5) # вторым аргументом указываем до какого знака после точки округлять
+# print (z) # round - округление, принимает число и аргкмент (до какого знака после точки нужно округлить)
+# # print (f'{z:,}') # переносим запятую на три знака влево
+# print (f'{z:.2f}') # переносим запятую на три знака влево при делении
+
+# def hello(name):
+#    print('hello', name)
+
+# name = input('tell your name ')
+
+# hello(name)
+
+def main():
+   name = input('tell your name ')
+   hello(name)
+
+def hello(name):
+   print('hello', name)
+
+main()
