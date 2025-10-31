9cc7c6394eb0575c19541b8054a9baf9e12abbc4
Ivan Ushakov <Ivanov.ivan.krd@gmail.com>
2025-10-31 19:03:39 +0300

    end of Lesson_1

---
+++ Lessons/Lesson_1.py
@@ -0,0 +1,65 @@
+# conditionals - условные операторы
+'''
+использовать else нужно
+
+# '''
+# score = int(input('Score: '))
+# if 90 <= score <= 100:
+#    print('A')
+# elif 80 <= score < 90:
+#    print('b')
+# elif 70 <= score < 80:
+#    print('c')
+# elif 60 <= score < 70:
+#    print('d')
+# else:
+#    print('f')
+
+# score = int(input('Score: '))
+# if score >= 90:
+#    print('A')
+# elif score >= 80:
+#    print('b')
+# elif score >= 70:
+#    print('c')
+# elif score >= 60:
+#    print('d')
+# else:
+#    print('f')
+
+# name = input('whats name? ')
+# if name == 'ivan' or name =='nastya':
+#    print('cool')
+# elif name == 'kostya':
+#    print('loh')
+# else: 
+#    print('who is it?')
+
+
+# # match - почти то же самое, что и if/elif/match но вроде удобнее
+# name = input('whats name? ')
+# match name:
+#    case 'ivan'| 'fedya' | 'nadya':
+#       print('cool')
+#    case 'nastya':
+#       print('hello')
+#    case 'kostya':
+#       print('loh')
+#    case _:
+#       print('who is it? ')
+
+
+# for _ in range(3): 
+#    print('hello')
+'''
+в range указываем сколько итераций должно быть (возвращает диапазон значений, ожидает аргумент
+это число = число значений выходных данных) '_' - используем, если переменная дальше не будет исспользоваться,
+а нужна только для того, чтобы цикл завелся
+'''
+
+print('hello\n' * 3, end='') # умножаем что нам надо вывести без циклов, \n чтобы был перенос на след строку, end= использовали для того, чтобы убрать 
+# стандартную пустую строку при выходе 
+
+
+
+
